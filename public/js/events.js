3a65e546d23d681cc4ab11b561e31b5f9bb5b402
gabrielamf <glennygabb@gmail.com>
2018-02-23 16:22:38 -0500

    añade vista states

--- public/js/events.js
+++ public/js/events.js
@@ -15,20 +15,21 @@ events.on('value', function(datos) {
   data = datos.val();
   $.each(data, function(indice, valor) {
     var template = 
-    `<div>
-      <p>${valor.title}</p>
-      <p class="opcion">Pendiente</p>
-      <select class="list" name="estado">
+    `<div class="card center-align">
+      <p class="center-align titleState">${valor.title}</p>
+      <p class="opcion">Estado: <span class="estado">Pendiente</span></p>
+      <select class="select list" name="estado">
         <option data-stage='Pendiente' value="Pendiente">Pendiente</option>
-        <option data-stage='cotización' value="cotización">cotización</option>
+        <option data-stage='Cotización' value="Cotización">Cotización</option>
         <option data-stage='OC proceso' value="OC proceso">OC proceso</option>
-        <option data-stage='revisión informe' value="revisión informe">revisión informe</option>
-        <option data-stage='Hes' value="Hes">Hes</option>
-        <option data-stage='correcciones' value="correcciones">correcciones</option>
-        <option data-stage='finalizado' value="finalizado">finalizado</option>
+        <option data-stage='Revisión de informe' value="Revisión de informe">Revisión informe</option>
+        <option data-stage='HES' value="Hes">HES</option>
+        <option data-stage='Correcciones' value="Correcciones">Correcciones</option>
+        <option data-stage='Finalizado' value="Finalizado">Finalizado</option>
       </select>
     </div>`;
     box.append(template);
+
   });
   $('.list').change(function() {
     // console.log(event.target);
